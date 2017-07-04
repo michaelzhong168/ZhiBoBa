0675701817ecec08ffb653a61a3a3cb9246a599f
钟 佳林 <zjl@ustockex.com>
2017-07-04 20:20:42 +0800

    添加第三方库

---
+++ ZhiBoBa/ZhiBoBa/Expand/Common/AppManager.swift
@@ -0,0 +1,19 @@
+//
+//  AppManager.swift
+//  ZhiBoBa
+//
+//  Created by 钟 佳林 on 2017/5/22.
+//  Copyright © 2017年 钟 佳林. All rights reserved.
+//
+
+import UIKit
+
+
+let APP = AppManager.shareInstance
+
+class AppManager: NSObject {
+
+
+    static let shareInstance = AppManager()
+
+}
--- ZhiBoBa/ZhiBoBa/Expand/Common/BaseClass/ZBBaseViewController.swift
+++ ZhiBoBa/ZhiBoBa/Expand/Common/BaseClass/ZBBaseViewController.swift
@@ -1,25 +1,27 @@
 //
-//  ViewController.swift
+//  ZBBaseViewController.swift
 //  ZhiBoBa
 //
-//  Created by 钟 佳林 on 2017/5/17.
+//  Created by 钟 佳林 on 2017/5/19.
 //  Copyright © 2017年 钟 佳林. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ZBBaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
-
